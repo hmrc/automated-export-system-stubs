3b6836dd453e07053f80092fd66073744015c382
Abigail Houston <abigail.houston1@digital.hmrc.gov.uk>
2026-09-07 14:30:15 +0100

    AES-231: Added the remainder of the IE906 rules

--- app/uk/gov/hmrc/automatedexportsystemstubs/errors/IE906Rules.scala
+++ app/uk/gov/hmrc/automatedexportsystemstubs/errors/IE906Rules.scala
@@ -21,10 +21,33 @@ import uk.gov.hmrc.automatedexportsystemstubs.utils.AsyncErrorResponseHelper.*
 object IE906Rules:
   val all: List[XmlRule] = List(
     XmlRule("8", "GoodsShipment.Consignment.ReferenceNumberUCRID", extractReferenceNumberUCRID, endsWith("000")),
+
+    XmlRule("12", "GoodsShipment.Consignment.LocationOfGoods.typeOfLocation", extractTypeOfLocation, equalsTo("E")),
+
+    XmlRule("13", "GoodsShipment.Consignment.LocationOfGoods.authorisationNumber", extractAuthorisationNumber, equalsTo("1104")),
+    XmlRule("14", "GoodsShipment.Consignment.LocationOfGoods.authorisationNumber", extractAuthorisationNumber, equalsTo("1103")),
+    XmlRule("15", "GoodsShipment.Consignment.LocationOfGoods.authorisationNumber", extractAuthorisationNumber, equalsTo("1100")),
+    XmlRule("26", "GoodsShipment.Consignment.LocationOfGoods.authorisationNumber", extractAuthorisationNumber, equalsTo("1101")),
     XmlRule("27", "GoodsShipment.Consignment.LocationOfGoods.authorisationNumber", extractAuthorisationNumber, equalsTo("1102")),
-    XmlRule("35", "GoodsShipment.Consignment.parentUCRID", extractParentUcrId, endsWith("AA")),
+
+    XmlRule("35", "GoodsShipment.Consignment.parentUCRID", extractParentUcrId, endsWith("A1")),
+
+    XmlRule("50", "ExportOperation.MRN", extractMrn, endsWith("B3")),
+    XmlRule("51", "ExportOperation.MRN", extractMrn, endsWith("B2")),
+    XmlRule("52", "ExportOperation.MRN", extractMrn, endsWith("B1")),
     XmlRule("90", "ExportOperation.MRN", extractMrn, endsWith("B0")),
-    XmlRule("93", "ExportOperation.MRN", extractMrn, endsWith("B1")),
+
+    XmlRule("92", "GoodsShipment.Consignment.TransportEquipment.sequenceNumber", extractTransportEquipmentSequenceNumber, equalsTo("40")),
+    XmlRule("92", "GoodsShipment.Consignment.TransportEquipment.Seal.sequenceNumber", extractSealSequenceNumber, equalsTo("40")),
+    XmlRule("92", "GoodsShipment.Consignment.TransportEquipment.GoodsReference.sequenceNumber", extractGoodsReferenceSequenceNumber, equalsTo("40")),
+    XmlRule("92", "GoodsShipment.Consignment.LocationOfGoods.sequenceNumber", extractLocationOfGoodsSequenceNumber, equalsTo("40")),
+    XmlRule("92", "GoodsShipment.Consignment.TransportDocument.sequenceNumber", extractTransportDocumentSequenceNumber, equalsTo("40")),
+    XmlRule("92", "GoodsShipment.GoodsItem.Commodity.Packaging.sequenceNumber", extractPackagingSequenceNumber, equalsTo("40")),
+
+    XmlRule("93", "ExportOperation.MRN", extractMrn, endsWith("A4")),
+    XmlRule("94", "ExportOperation.MRN", extractMrn, endsWith("B4")),
+    XmlRule("95", "ExportOperation.MRN", extractMrn, endsWith("B5")),
+
     XmlRule("96", "GoodsShipment.Consignment.ReferenceNumberUCRID", extractReferenceNumberUCRID, endsWith("001")),
     XmlRule("97", "GoodsShipment.Consignment.ReferenceNumberUCRID", extractReferenceNumberUCRID, endsWith("002")),
     XmlRule("98", "GoodsShipment.Consignment.ReferenceNumberUCRID", extractReferenceNumberUCRID, endsWith("003")),
--- app/uk/gov/hmrc/automatedexportsystemstubs/utils/AsyncErrorResponseHelper.scala
+++ app/uk/gov/hmrc/automatedexportsystemstubs/utils/AsyncErrorResponseHelper.scala
@@ -29,14 +29,40 @@ object AsyncErrorResponseHelper:
   def textAt(ns: NodeSeq): Option[String] =
     ns.headOption.map(_.text).map(_.trim).filter(_.nonEmpty)
 
-  def extractMrn(xml: Elem): Option[String] =
-    textAt(xml \\ "ExportOperation" \\ "MRN")
+  def extract(path: String)(xml: Elem): Option[String] =
+    val names = path.split('.').toList
+    val nodes = names.foldLeft(xml: NodeSeq)((acc, name) => acc \\ name)
+    textAt(nodes)
 
-  def extractReferenceNumberUCRID(xml: Elem): Option[String] =
-    textAt(xml \\ "GoodsShipment" \\ "Consignment" \\ "ReferenceNumberUCRID")
+  val extractMrn: Elem => Option[String] =
+    extract("ExportOperation.MRN")
 
-  def extractAuthorisationNumber(xml: Elem): Option[String] =
-    textAt(xml \\ "GoodsShipment" \\ "Consignment" \\ "LocationOfGoods" \\ "authorisationNumber")
+  val extractReferenceNumberUCRID: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.ReferenceNumberUCRID")
 
-  def extractParentUcrId(xml: Elem): Option[String] =
-    textAt(xml \\ "GoodsShipment" \\ "Consignment" \\ "parentUCRID")
+  val extractAuthorisationNumber: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.LocationOfGoods.authorisationNumber")
+
+  val extractParentUcrId: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.parentUCRID")
+
+  val extractTypeOfLocation: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.LocationOfGoods.typeOfLocation")
+
+  val extractTransportEquipmentSequenceNumber: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.TransportEquipment.sequenceNumber")
+
+  val extractSealSequenceNumber: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.TransportEquipment.Seal.sequenceNumber")
+
+  val extractGoodsReferenceSequenceNumber: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.TransportEquipment.GoodsReference.sequenceNumber")
+
+  val extractLocationOfGoodsSequenceNumber: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.LocationOfGoods.sequenceNumber")
+
+  val extractTransportDocumentSequenceNumber: Elem => Option[String] =
+    extract("GoodsShipment.Consignment.TransportDocument.sequenceNumber")
+
+  val extractPackagingSequenceNumber: Elem => Option[String] =
+    extract("GoodsShipment.GoodsItem.Commodity.Packaging.sequenceNumber")
